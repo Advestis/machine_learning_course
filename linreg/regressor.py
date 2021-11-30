9362ad2b67826edc3243571fd13a9d0ee1a36abd
pcotte <pcotte@advestis.com>
2021-11-30 09:41:53 +0100

    chg: obfuscated

--- linreg/regressor.py
+++ linreg/regressor.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 
 def check_expected_kwargs(kwargs, expected, allow_unexpected: bool = True):
@@ -20,13 +19,6 @@ def compute_rsquared(y, pred_y):
     return 1 - (rss / tss)
 
 
-def normalise(values):
-    mean = values.mean()
-    std = values.std()
-    values_normalied = (values - mean) / std
-    return values_normalied, mean, std
-
-
 class LinearRegressor:
     def __init__(self, x: pd.Series, y: pd.Series):
 
@@ -38,9 +30,7 @@ class LinearRegressor:
         if isinstance(y, (pd.DataFrame, pd.Series)):
             y = y.values
         self.x = x
-        self.x_norm, self.xmean, self.xstd = normalise(self.x)
         self.y = y
-        self.y_norm, self.ymean, self.ystd = normalise(self.y)
         self.pred_y = None
         self.n = len(x)
         print(f"There are {self.n} observations")
@@ -48,24 +38,12 @@ class LinearRegressor:
         self.a = None
         self.b = None
         self.max_epoch = 1e4
-        self.alpha = 0.001
+        self.learning_rate = 0.001
         self.converged = None
         self.expected_y_std = None
         self.stopping_criterion = None
         self.learning_summary = pd.DataFrame(columns=["loss", "a", "b", "ga", "gb"], dtype=float)
-        self.learning_summary_norm = pd.DataFrame(columns=["loss", "a", "b", "ga", "gb"], dtype=float)
         self.learning_summary.index.name = "epoch"
-
-    def convert_params(self, a, b, to="normalized"):
-        if to == "normalized":
-            aa = a * self.xstd / self.ystd
-            bb = (b - self.ymean) / self.ystd + aa * self.xmean / self.xstd
-        elif to == "real":
-            aa = a * self.ystd / self.xstd
-            bb = self.ymean + self.ystd * (b - self.xmean * a / self.xstd)
-        else:
-            raise ValueError(f"Invalid parameter '{to}' for 'normalized'")
-        return aa, bb
 
     def fit(self, method="analytic", **kwargs):
         if method == "analytic":
@@ -78,66 +56,45 @@ class LinearRegressor:
 
     def fit_analytic(self):
         print(f"fitting...")
-        print("sum of y:", self.y.sum())
-        print("mean of y:", self.y.mean())
-        print("y divided by 2:", self.y / 2)
         """ Code here """
-        ss_xy = (self.y * self.x).sum() - self.x.sum() * self.y.sum() / self.n
-        ss_xx = (self.x * self.x).sum() - (self.x.sum() ** 2) / self.n
-        self.a = ss_xy / ss_xx
-        self.b = self.y.mean() - self.a * self.x.mean()
         self._finish_learning()
 
     def fit_numeric(self, theta=None):
         def one_fit(a_, b_, x_, y_):
-            y_model = a_ * x_ + b_
-            diffs = y_ - y_model
-            loss = np.sum(diffs ** 2) / (2 * self.n)
+            y_model =
+            diffs =
+            loss =
             if np.isinf(loss) or np.isnan(loss):
                 raise ValueError("loss is invalid")
-            g_a = (-1 / self.n) * (x_ * diffs).sum()
-            g_b = (-1 / self.n) * diffs.sum()
+            g_a =
+            g_b =
             return loss, g_a, g_b
 
         print(f"fitting...")
         """ Code here """
 
         i = 0
-        a_real, b_real = theta
-        a_norm, b_norm = self.convert_params(a_real, b_real, to="normalized")
+        a, b = theta
 
-        if self.max_epoch is None and self.stopping_criterion is None:
-            raise ValueError(
-                "At least one of max_iterations or loss_precicion_stop must be set for LinearRegressor if "
-                "using numeric fit."
-            )
+        if self.max_epoch is None:
+            raise ValueError("max_epoch can not be None")
         while self.max_epoch is None or i < self.max_epoch:
             try:
-                self.a = a_real
-                self.b = b_real
-                loss_real, g_a_real, g_b_real = one_fit(a_real, b_real, self.x, self.y)
-                loss_norm, g_a_norm, g_b_norm = one_fit(a_norm, b_norm, self.x_norm, self.y_norm)
-                print(f"  a (real, normalized): {self.a}, {a_norm} -- b (real, normalized): {self.b}, {b_norm}")
-                print(f"Epoch {i} | loss (real, normalized coordinates): {loss_real}, {loss_norm}")
-                if np.isinf(loss_real) or np.isnan(loss_real) or np.isinf(loss_norm) or np.isnan(loss_norm):
+                self.a =
+                self.b =
+                loss, g_a, g_b =
+                print(f"  a: {self.a} -- b: {self.b}")
+                print(f"Epoch {i} | loss {loss}")
+                if np.isinf(loss) or np.isnan(loss):
                     raise ValueError("loss is invalid")
 
-                self.learning_summary_norm.loc[i] = [loss_norm, a_norm, b_norm, g_a_norm, g_b_norm]
-                self.learning_summary.loc[i] = [loss_real, self.a, self.b, g_a_real, g_b_real]
-                self._check_convergence()
-                if self.converged:
-                    break
-
-                i += 1
-                a_real = a_real - self.alpha * g_a_real
-                b_real = b_real - self.alpha * g_b_real
-                a_norm = a_norm - self.alpha * g_a_norm
-                b_norm = b_norm - self.alpha * g_b_norm
+                self.learning_summary.loc[i] = [loss, self.a, self.b, g_a, g_b]
+                i =
+                a =
+                b =
             except Exception as e:
                 print(f"Stopping the gradient descent because an error occured : {str(e)}")
                 break
-        if not self.converged:
-            print("The learning did not converge")
         self._finish_learning()
 
     def _finish_learning(self):
@@ -145,9 +102,6 @@ class LinearRegressor:
         self.rsquared = compute_rsquared(self.y, self.pred_y)
         print(f"...done. Results are : \n - a={self.a}\n - b={self.b}\n - r**2={self.rsquared}")
 
-    def _check_convergence(self, *args):
-        """To implement"""
-        self.converged = False
 
     def plot(self, path: str = None):
         print(f"plotting...")
@@ -168,17 +122,14 @@ class LinearRegressor:
             print(f"...figure saved in {path}")
             plt.close("all")  # Without this command, all future plots will be made in the same figure
         else:
-            return plt.gcf()
+            return plt.gcf()  # gcf means "get current figure"
 
-    def plot_summary(self, path: str = None, which="real"):
+    def plot_summary(self, path: str = None):
         if self.learning_summary.empty:
             print("Learning summary is empty")
             return
 
-        if which == "real":
-            df = self.learning_summary
-        else:
-            df = self.learning_summary_norm
+        df = self.learning_summary
 
         fig, axis = plt.subplots(2, 2, figsize=(15, 9))
         fig.suptitle("Learning summary", fontsize=25)
@@ -214,185 +165,5 @@ class LinearRegressor:
         else:
             return fig
 
-    def save_summary(self, path, which="real"):
-        if which == "real":
-            self.learning_summary.to_csv(path)
-        else:
-            self.learning_summary_norm.to_csv(path)
-
-    def animate(self, path: str = None, correct_a: float = None, correct_b: float = None, step=20, nbins=100):
-        def get_loss(a_, b_, x_, y_):
-            ax = np.outer(a_, x_)
-            diff = np.array([ax - y_ + sb for sb in b_])
-            loss = np.sum(diff ** 2, axis=2) / (2 * self.n)
-            return loss
-
-        def make_range(real, col):
-            min_ = min(min(col), real)
-            max_ = max(max(col), real)
-
-            dist_to_min = abs(real - min_)
-            dist_to_max = abs(real - max_)
-
-            if dist_to_min > dist_to_max:
-                max_ = real + dist_to_min
-            else:
-                min_ = real - dist_to_max
-            return [min_, max_]
-
-        if correct_a is None:
-            correct_a = self.a
-        if correct_b is None:
-            correct_b = self.b
-
-        correct_a_norm, correct_b_norm = self.convert_params(correct_a, correct_b, to="normalized")
-
-        a_real = self.learning_summary.iloc[0]["a"]
-        b_real = self.learning_summary.iloc[0]["b"]
-        a_norm = self.learning_summary_norm.iloc[0]["a"]
-        b_norm = self.learning_summary_norm.iloc[0]["b"]
-
-        # g_a_real = self.learning_summary.loc[0, "ga"]
-        # g_b_real = self.learning_summary.loc[0, "gb"]
-        # g_a_norm = self.learning_summary_norm.loc[0, "ga"]
-        # g_b_norm = self.learning_summary_norm.loc[0, "gb"]
-
-        a_range_real = make_range(correct_a, self.learning_summary["a"])
-        b_range_real = make_range(correct_b, self.learning_summary["b"])
-        a_range_norm = make_range(correct_a_norm, self.learning_summary_norm["a"])
-        b_range_norm = make_range(correct_b_norm, self.learning_summary_norm["b"])
-        slopes_real = np.linspace(*a_range_real, nbins)
-        slopes_norm = np.linspace(*a_range_norm, nbins)
-        intercepts_real = np.linspace(*b_range_real, nbins)
-        intercepts_norm = np.linspace(*b_range_norm, nbins)
-        slopes_grid_real, intercepts_grid_real = np.meshgrid(slopes_real, intercepts_real)
-        slopes_grid_norm, intercepts_grid_norm = np.meshgrid(slopes_norm, intercepts_norm)
-        losses_real = get_loss(slopes_real, intercepts_real, self.x, self.y)
-        losses_norm = get_loss(slopes_norm, intercepts_norm, self.x_norm, self.y_norm)
-
-        fig = plt.figure(figsize=(15, 15))
-        axes = [
-            fig.add_subplot(2, 2, 1),
-            fig.add_subplot(2, 2, 2, projection="3d"),
-            fig.add_subplot(2, 2, 3),
-            fig.add_subplot(2, 2, 4, projection="3d"),
-        ]
-
-        p_real = axes[0].scatter(self.x, self.y, color="blue", marker="o", s=10)
-        axes[0].grid()
-        line_real = axes[0].plot(
-            self.x,
-            a_real * self.x + b_real,
-            color="red",
-        )[0]
-        tb = f" + {round(b_real, 3)}" if b_real >= 0 else f"-{round(b_real, 3)}"
-        legend_real = axes[0].text(0.1, 0.95, f"Epoch 0: {round(a_real, 3)}x{tb}", transform=axes[0].transAxes)
-        axes[0].set_xlabel("x")
-        axes[0].set_ylabel("y")
-
-        p_norm = axes[2].scatter(self.x, self.y, color="blue", marker="o", s=10)
-        axes[2].grid()
-        line_norm = axes[2].plot(
-            self.x,
-            a_real * self.x + b_real,
-            color="red",
-        )[0]
-        tb = f" + {round(b_norm, 3)}" if b_norm >= 0 else f"-{round(b_norm, 3)}"
-        legend_norm = axes[2].text(0.1, 0.95, f"Epoch 0: {round(a_norm, 3)}x{tb}", transform=axes[2].transAxes)
-        axes[2].set_xlabel("x")
-        axes[2].set_ylabel("y")
-
-        axes[1].plot_surface(
-            slopes_grid_real, intercepts_grid_real, losses_real, alpha=0.5, antialiased=True, cmap="terrain"
-        )
-        axes[1].set_xlabel("a")
-        axes[1].set_ylabel("b")
-        axes[1].set_zlabel("loss")
-        axes[1].scatter(
-            [slopes_real[0]],
-            [intercepts_real[0]],
-            [losses_real[0][0]],
-            marker="x",
-            color="red",
-            s=10,
-        )
-
-        axes[3].plot_surface(
-            slopes_grid_norm, intercepts_grid_norm, losses_norm, alpha=0.5, antialiased=True, cmap="terrain"
-        )
-        axes[3].set_xlabel("a_N")
-        axes[3].set_ylabel("b_N")
-        axes[3].set_zlabel("loss_N")
-        axes[3].scatter(
-            [slopes_norm[0]],
-            [intercepts_norm[0]],
-            [losses_norm[0][0]],
-            marker="x",
-            color="red",
-            s=10,
-        )
-
-        def update(i):
-            print(f"Animating {i}...")
-            a_real_ = self.learning_summary.loc[i, "a"]
-            b_real_ = self.learning_summary.loc[i, "b"]
-            a_norm_ = self.learning_summary_norm.loc[i, "a"]
-            b_norm_ = self.learning_summary_norm.loc[i, "b"]
-
-            # g_a_real_ = self.learning_summary.loc[i, "ga"]
-            # g_b_real_ = self.learning_summary.loc[i, "gb"]
-            # g_a_norm_ = self.learning_summary_norm.loc[i, "ga"]
-            # g_b_norm_ = self.learning_summary_norm.loc[i, "gb"]
-
-            pred_real = a_real_ * self.x + b_real_
-            a_norm_real, b_norm_real = self.convert_params(a_norm_, b_norm_, to="real")
-            pred_norm_real = a_norm_real * self.x + b_norm_real
-            line_real.set_ydata(pred_real)
-            line_norm.set_ydata(pred_norm_real)
-            tb_ = f" + {round(b_real_, 3)}" if b_real_ > 0 else f"-{round(b_real_, 3)}"
-            legend_real.set_text(f"Epoch {i}: {round(a_real_, 3)}x{tb_}")
-            tb_ = f" + {round(b_norm_real, 3)}" if b_norm_real > 0 else f"-{round(b_norm_real, 3)}"
-            legend_norm.set_text(f"Epoch {i}: {round(a_norm_real, 3)}x{tb_}")
-
-            loss_real = self.learning_summary.loc[i, "loss"]
-            loss_norm = self.learning_summary_norm.loc[i, "loss"]
-
-            # loss_plus_nabla_real = get_loss(
-            #     [a_real_ + self.alpha * g_b_real_], [b_real_ - self.alpha * g_a_real_], self.x, self.y
-            # )
-            # loss_plus_nabla_norm = get_loss(
-            #     [a_norm_ + self.alpha * g_b_norm_], [b_norm_ - self.alpha * g_a_norm_], self.x_norm, self.y_norm
-            # )
-            #
-            # print("real:", loss_plus_nabla_real, self.learning_summary.loc[i, "loss"])
-            # print("norm:", loss_plus_nabla_norm, self.learning_summary_norm.loc[i, "loss"])
-
-            axes[1].scatter(
-                [a_real_],
-                [b_real_],
-                [loss_real],
-                marker="x",
-                color="red",
-                s=10,
-            )
-
-            axes[3].scatter(
-                [a_norm_],
-                [b_norm_],
-                [loss_norm],
-                marker="x",
-                color="red",
-                s=10,
-            )
-            return p_real, p_norm, line_real, line_norm, legend_real, legend_norm
-
-        # noinspection PyTypeChecker
-        ani = animation.FuncAnimation(fig, update, blit=True, frames=self.learning_summary.index[1::step])
-        plt.title("Top : Real coordinates. Bottom : normalized coordinates.")
-
-        if path is not None:
-            ani.save(path, fps=20, dpi=300)
-            print(f"...animation saved in {path}")
-            plt.close("all")  # Without this command, all future plots will be made in the same figure
-        else:
-            return ani
+    def save_summary(self, path):
+        self.learning_summary.to_csv(path)
